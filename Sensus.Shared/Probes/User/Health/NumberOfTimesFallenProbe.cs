80596fc1986fc141d6930d1124896af6b9d127b1
Benjamin Davidson <bzd3y@virginia.edu>
2019-06-10 13:49:02 -0400

    shared health probes

---
+++ Sensus.Shared/Probes/User/Health/NumberOfTimesFallenProbe.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2014 The Rector & Visitors of the University of Virginia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sensus.Probes.User.Health
+{
+	public abstract class NumberOfTimesFallenProbe : PollingProbe
+	{
+		public sealed override string DisplayName
+		{
+			get
+			{
+				return "Number of Times Fallen";
+			}
+		}
+
+		public override Type DatumType
+		{
+			get
+			{
+				return typeof(NumberOfTimesFallenDatum);
+			}
+		}
+
+		public override int DefaultPollingSleepDurationMS
+		{
+			get
+			{
+				return (int)TimeSpan.FromDays(5).TotalMilliseconds;
+			}
+		}
+
+		public NumberOfTimesFallenProbe() : base()
+		{
+
+		}
+
+		protected override Task<List<Datum>> PollAsync(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
