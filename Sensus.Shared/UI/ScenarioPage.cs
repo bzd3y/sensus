303d1c3513df9e274188e9fd753ac81dd21e327e
emilyleventhal <ell2yvj@virginia.edu>
2020-07-22 18:29:12 -0400

    ScenarioPage.cs completed

--- Sensus.Shared/UI/ScenarioPage.cs
+++ Sensus.Shared/UI/ScenarioPage.cs
@@ -23,9 +23,7 @@ namespace Sensus.UI
 {
     public class ScenarioPage : ContentPage
     {
-        protected Grid _contentGrid;
         protected RelativeLayout _contentLayout;
-        protected ListView _notificationList;
 
         public ScenarioPage()
         {
@@ -49,6 +47,33 @@ namespace Sensus.UI
                 // eventually add MTlogo + "Session 2" label 
             };
 
+            RelativeLayout bannerLayout = new RelativeLayout();
+
+            Image logoImage = new Image { Source = "Logo.png" };
+            Label sessionNum = new Label
+            {
+                Text = "Session 2",
+                TextColor = Color.White,
+                FontFamily = "Source Sans Pro",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 30
+            };
+            bannerLayout.Children.Add(logoImage,
+                xConstraint: Constraint.RelativeToParent((parent) =>
+                    { return parent.Width * .01; }),
+                yConstraint: Constraint.RelativeToParent((parent) =>
+                    { return parent.Height * .3; }),
+                widthConstraint: Constraint.RelativeToParent((parent) =>
+                    { return parent.Width *.2; }));
+
+            bannerLayout.Children.Add(sessionNum,
+                xConstraint: Constraint.RelativeToView(logoImage,
+                    (parent, sibling) => { return sibling.Width * 1.2; }),
+                yConstraint: Constraint.RelativeToParent((parent) =>
+                    { return parent.Height - 40; }));
+
+            bannerFrame.Content = bannerLayout;
+
             Frame whiteFrame = new Frame
             {
                 BackgroundColor = Color.White,
@@ -60,20 +85,118 @@ namespace Sensus.UI
                 VerticalOptions = LayoutOptions.Center,
             };
 
-            _contentLayout.Children.Add(whiteFrame,
-                heightConstraint: Constraint.RelativeToParent(
-                    (parent) => { return parent.Height; }),
-                widthConstraint: Constraint.RelativeToParent(
-                    (parent) => { return parent.Width; }));
+
             _contentLayout.Children.Add(bannerFrame,
                 heightConstraint: Constraint.RelativeToParent((parent) =>
                 { return parent.Height * 0.16; }),
                 widthConstraint: Constraint.RelativeToParent((parent) =>
                 { return parent.Width; }));
-            // if this isn't working, try: 
-            // widthConstraint:Constraint.RelativeToParent(parent => parent.Width));
+
+            _contentLayout.Children.Add(whiteFrame,
+                heightConstraint: Constraint.RelativeToParent(
+                    (parent) => { return parent.Height * .75; }),
+                widthConstraint: Constraint.RelativeToView(bannerFrame,
+                    (parent, sibling) => { return sibling.Width *.8; }),
+                xConstraint: Constraint.RelativeToParent(
+                    (parent) => { return parent.Width * .1; }),
+                yConstraint: Constraint.RelativeToView(bannerFrame,
+                    (parent, sibling) => { return sibling.Height + 30; }));
+
+            StackLayout frameLayout = new StackLayout
+            {
+                Padding = new Thickness(20, 15)
+            };
+
+            whiteFrame.Content = frameLayout; // set the content of a frame
+
+            Grid headerGrid = new Grid
+            {
+                ColumnSpacing = 0,
+                RowSpacing = 0,
+                Padding = 0,
+                ColumnDefinitions = {
+                    new ColumnDefinition {
+                        Width = new GridLength(1, GridUnitType.Star)
+                    },
+                    new ColumnDefinition
+                    {
+                        Width = new GridLength(3, GridUnitType.Star)
+                    }
+                },
+
+            };
+            Label scenarioNum = new Label {
+                Text = "Scenario 1",
+                Margin = new Thickness(10),
+                TextColor = Color.FromHex("166DA3"),
+                FontSize = 22,
+                FontFamily = "Source Sans Pro",
+                HorizontalTextAlignment = TextAlignment.Start
+                // WidthRequest =1000
+
+            };
+            Image scenarioIcon = new Image {Source = "pencil.png", HeightRequest = 10 };
+
+            headerGrid.Children.Add(scenarioIcon, 0, 0);
+
+            headerGrid.Children.Add(scenarioNum, 1, 0); // column, row 
+            
+            frameLayout.Children.Add(headerGrid);
+
+            Frame grayFrame = new Frame
+            {
+                BackgroundColor = Color.FromHex("F0ECEC"),
+                HasShadow = false,
+                Padding =0,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center,
+                WidthRequest = 250,
+                CornerRadius = 10
+            };
+
+            Label scenarioName = new Label
+            {
+                Text = "Writing a report",
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                FontSize = 25,
+                TextColor = Color.Black,
+                HeightRequest = 100
+            };
+            Image scenarioImage = new Image {
+                Source = "Report.png",
+                HeightRequest=200,
+                Margin = new Thickness(0,20,0,0)};
+
+            grayFrame.Content = scenarioName;
+            frameLayout.Children.Add(grayFrame);
+            frameLayout.Children.Add(scenarioImage);
+
+            Button nextButton = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.EndAndExpand,
+                Text = "Next",
+                TextColor = Color.White,
+                BackgroundColor = Color.FromHex("166DA3"),
+                FontAttributes = FontAttributes.Bold,
+                FontFamily = "Source Sans Pro",
+                Margin = new Thickness(10),
+                WidthRequest = 150,
+                CornerRadius = 8,
+                FontSize = 20
+            };
 
 
+            frameLayout.Children.Add(nextButton);
+
+            ProgressBar sessionProgress = new ProgressBar
+            {
+                Progress = .2,
+                ProgressColor = Color.FromHex("166DA3")
+            };
+
+            frameLayout.Children.Add(sessionProgress);
         }
 
     }
